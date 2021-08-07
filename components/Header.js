f6924e0e81e329193a5db65ff8336bb7816f1ad1
Elliop <abdelmouhayminegx@gmail.com>
2021-08-08 00:41:16 +0100

    Building the Center of the Header

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import HeaderIcon from "./HeaderIcon";
 import {
   BellIcon,
   ChatIcon,
@@ -11,12 +12,12 @@ import {
   FlagIcon,
   PlayIcon,
   SearchIcon,
-  ShoppingCardIcon,
+  ShoppingCartIcon,
 } from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* Left */}
       <div className="flex items-center">
         <Image
@@ -25,18 +26,25 @@ function Header() {
           height={40}
           layout="fixed"
         />
-
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
             type="text"
             placeholder="Search Facebook"
           ></input>
         </div>
       </div>
       {/* Center */}
-
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
       {/* Right */}
     </div>
   );
