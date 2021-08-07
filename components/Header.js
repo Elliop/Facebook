b898161dab18a361080601eca3fa0905e1bca4fb
Elliop <abdelmouhayminegx@gmail.com>
2021-08-07 23:52:32 +0100

    Building the Left Side of the Header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,40 @@
 import Image from "next/image";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCardIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
     <div>
       {/* Left */}
+      <div className="flex items-center">
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
 
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500"
+            type="text"
+            placeholder="Search Facebook"
+          ></input>
+        </div>
+      </div>
       {/* Center */}
 
       {/* Right */}
