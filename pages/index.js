e2a0f2f175cc91aea6148f2b99c7ece8a0fd508d
Elliop <abdelmouhayminegx@gmail.com>
2021-08-08 01:55:58 +0100

    Implementing NextAuth.js to Log With Facebook

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import { getSession } from "next-auth/client";
 import Head from "next/head";
 import Header from "../components/Header";
 
-export default function Home() {
+export default function Home({ session }) {
   return (
     <div>
       <Head>
@@ -11,4 +12,15 @@ export default function Home() {
       <main></main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  // Get the user props
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
