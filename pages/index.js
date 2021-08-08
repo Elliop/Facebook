eb93760dfde694e605d579c728bd4bce24e2b412
Elliop <abdelmouhayminegx@gmail.com>
2021-08-08 02:14:49 +0100

    Building the Login Components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import { getSession } from "next-auth/client";
 import Head from "next/head";
 import Header from "../components/Header";
+import Login from "../components/Login";
 
 export default function Home({ session }) {
+  if (!session) return <Login />;
   return (
     <div>
       <Head>
