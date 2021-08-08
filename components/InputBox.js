76882f97066b84ef5c0c367be4c5aca17590ca7e
Elliop <abdelmouhayminegx@gmail.com>
2021-08-08 17:11:12 +0100

    Store simple Text Message in the Database

--- components/InputBox.js
+++ components/InputBox.js
@@ -2,12 +2,29 @@ import { useSession } from "next-auth/client";
 import Image from "next/image";
 import { EmojiHappyIcon } from "@heroicons/react/outline";
 import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
+import { useRef } from "react";
+import { db } from "../firebase";
+import firebase from "firebase";
 
 function InputBox() {
   const [session, loading] = useSession();
+  const inputRef = useRef(null);
 
   const sendPost = (e) => {
     e.preventDefault();
+
+    if (!inputRef.current.value) return;
+
+    db.collection("posts").add({
+      message: inputRef.current.value,
+      name: session.user.name,
+      email: session.user.email,
+      image: session.user.image,
+      // Real Time From Firebase
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    inputRef.current.value = "";
   };
 
   return (
@@ -28,6 +45,7 @@ function InputBox() {
             className="rounded-full h-12 bg-gray-100
           flex-grow px-5 focus:outline-none"
             type="text"
+            ref={inputRef}
             placeholder={`What's on your mind, ${session.user.name}?`}
           />
           <button hidden type="submit" onClick={sendPost}>
