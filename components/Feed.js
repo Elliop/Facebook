0e2e11e2fd2c5d2e0c3df73f69d9c5f861fabd7a
Elliop <abdelmouhayminegx@gmail.com>
2021-08-08 19:40:06 +0100

    Buillding Widgets Component

--- components/Feed.js
+++ components/Feed.js
@@ -6,7 +6,7 @@ function Feed() {
   return (
     <div
       className="flex-grow h-screen pb-44 pt-6 mr-4
-     xl:mr-40 overflow-y-auto"
+     xl:mr-40 overflow-y-auto scrollbar-hide"
     >
       <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
         <Stories />
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Login from "../components/Login";
 import Sidebar from "../components/Sidebar";
 import Feed from "../components/Feed";
+import Widgets from "../components/Widgets";
 
 export default function Home({ session }) {
   if (!session) return <Login />;
@@ -16,6 +17,7 @@ export default function Home({ session }) {
       <main className="flex">
         <Sidebar />
         <Feed />
+        <Widgets />
       </main>
     </div>
   );
